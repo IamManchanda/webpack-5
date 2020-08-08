8a921cc1b859861969980464c999a5d8f6462f7c
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-08-08 18:05:53 +0530

    Loading CSS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 
-import Webpack from "./assets/Webpack";
+import Webpack from "./components/Webpack";
+import "./assets/css/styles.css";
 
 const rootElement = document.getElementById("root");
 render(<Webpack />, rootElement);
